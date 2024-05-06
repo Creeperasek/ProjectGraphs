778bb2e1e8c17f8c935c2390e17e92067e7f373f
Creeperasek <76570152+Creeperasek@users.noreply.github.com>
2024-05-06 08:50:16 +0200

    Modified most of code Everything is now working i hope

--- draw.py
+++ draw.py
@@ -1,43 +1,52 @@
 from pyvis.network import Network
 
-g = Network()
+#Number of nodes
+n = 10
 
-file = open("graph.txt","r")
+#Drawing function - creates html file with drawn file
+def draw_graph(filename,graphfile):
+    g = Network()
 
-nodes = [0,1,2,3,4,5,6,7,8,9]
-g.add_nodes(nodes=nodes,
-            label=["0","1","2","3","4","5","6","7","8","9"],
-            color=["red","blue","green","brown","yellow","pink","purple","black","orange","dark_green"])
+    file = open(filename,"r")
 
-j = 0
-i= 0
+    nodes = [0,1,2,3,4,5,6,7,8,9]
+    g.add_nodes(nodes=nodes,
+                label=["0","1","2","3","4","5","6","7","8","9"],
+                color=["red","blue","green","brown","yellow","pink","purple","black","orange","dark_green"])
 
-for node in range(9):
-    line = file.readline()
-    line = line[:10]
-    edge=0
-    
-    for num in line:
-        if(edge >= j):
+    j = 0
+    i= 0
 
-            if(int(num) == 1):
-                g.add_edge(node,edge)
-        edge=edge+1
-    
-    j=j+1
+    for node in range(9):
+        line = file.readline()
+        line = line[:10]
+        edge=0
+        
+        for num in line:
+            if(edge >= j):
 
-
-i=0
-for _ in g.get_edges():
-    i=i+1
-print(i)
-
-
-print(g.get_adj_list())
+                if(int(num) == 1):
+                    g.add_edge(node,edge)
+            edge=edge+1
+        
+        j=j+1
 
 
+    i=0
+    for _ in g.get_edges():
+        i=i+1
+    print("Edges: "+ str(i)+"\n")
+    print("Adjective list:")
+    print(g.get_adj_list())
 
+    D = (2 * i) / (n * (n - 1))
+    print("\nGraph density = " + str(round(D,3)))
 
-g.hrepulsion(node_distance=500)
-g.toggle_physics(True)
-g.show('mygraph.html', True, False)
+    #g.hrepulsion(node_distance=500)
+    g.toggle_physics(True)
+    g.barnes_hut()
+    g.write_html(graphfile, True, False,False)
+
+#Main
+draw_graph("Initial_graph.txt","Initial_graph.html")
+draw_graph("Eulerian_graph.txt","Eulerian_graph.html")
--- main.py
+++ main.py
@@ -13,74 +13,46 @@ def generate_graph(n, p):
 def degree_sequence(A):
     return np.sum(A, axis=1)
 
-def order_odd_degrees(degrees):
-    odd_degrees = [(i, degree) for i, degree in enumerate(degrees) if degree % 2 != 0]
-    odd_degrees.sort(key=lambda x: x[1], reverse=True)
-    return [x[0] for x in odd_degrees]
+def has_eulerian_cycle(A):
+    return all(degree_sequence(A) % 2 == 0)
 
 def connect_vertices(A, i, j):
     A[i][j] = A[j][i] = 1
 
-def bfs_connected_component(graph, start):
-    visited = set()
-    queue = deque([start])
-    
-    while queue:
-        vertex = queue.popleft()
-        if vertex not in visited:
-            visited.add(vertex)
-            queue.extend(set(range(len(graph[vertex]))) - visited)
-    
-    return visited
+def disconnect_vertices(A, i, j):
+    A[i][j] = A[j][i] = 0
 
-def is_connected(graph):
-    visited = bfs_connected_component(graph, 0)
-    return len(visited) == len(graph)
+def find_vertices_with_odd_degrees(degrees):
+    return [i for i, degree in enumerate(degrees) if degree % 2 != 0]
 
 def make_eulerian(A):
-    if not is_connected(A):
-        print("Graph is not connected. Making the graph connected first.")
-        # Connect the graph before making it Eulerian
-        # This is a simple way to connect the graph. You might need a more complex logic depending on the graph.
-        for i in range(len(A) - 1):
-            connect_vertices(A, i, i + 1)
-    
-    degrees = degree_sequence(A)
-    odd_vertices = order_odd_degrees(degrees)
-    
-    while len(odd_vertices) > 1:
-        v1 = odd_vertices.pop()
-        v2 = odd_vertices.pop()
-        connect_vertices(A, v1, v2)
-    
+    while not has_eulerian_cycle(A):
+        degrees = degree_sequence(A)
+        odd_vertices = find_vertices_with_odd_degrees(degrees)
+        v1, v2 = random.sample(odd_vertices, 2)
+        if not A[v1][v2]:
+            connect_vertices(A, v1, v2)
+        else:
+            common_neighbors = [v for v in range(len(A)) if A[v1][v] and A[v2][v]]
+            if common_neighbors:
+                neighbor = random.choice(common_neighbors)
+                disconnect_vertices(A, v1, neighbor)
+                disconnect_vertices(A, v2, neighbor)
+            else:
+                disconnect_vertices(A, v1, v2)
     return A
 
-n = 10  # przykładowy rozmiar grafu
-p = 0.7  # przykładowe prawdopodobieństwo
-
-A = generate_graph(n, p)
-print("Initial graph:")
-print(A)
-
-if is_connected(A):
-    print("The graph is connected.")
-    A = make_eulerian(A)
-    print("Eulerian graph:")
-    print(A)
-
-    
+def create_graph_file(A,filename):
     array = A.tolist()
-
     i = 0
     j = 0
-
-    file = open("graph.txt", "w")
+    file = open(filename, "w")
     for line in array:
         j=j+1
         deg = 0
-        
-        for val in line:
             
+        for val in line:
+                
             file.write(str(val))
             deg += val
             i = i + 1
@@ -92,12 +64,28 @@ if is_connected(A):
                 file.write(f"| {str(deg)}")
                 file.write("\n")
                 i = 0
-                
+
     file.close()
     print('File Created Succesfuly')
 
-else:
-    print("The graph is not connected and cannot be made Eulerian.")
+n = 10  # przykładowy rozmiar grafu
+p = 0.7  # przykładowe prawdopodobieństwo
+
+#Create initial graph and save it to file
+A = generate_graph(n, p)
+print("Initial graph:")
+print(A)
+
+create_graph_file(A,"Initial_graph.txt")
+
+#Modify graph to be Eulerian and save it to file
+A = make_eulerian(A)
+print("Eulerian graph:")
+print(A)
+
+create_graph_file(A,"Eulerian_graph.txt")
+
+    
 
 
 
